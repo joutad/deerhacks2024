5e6fc8f7607d20c4b55c4b85e394c7cc4a17a4bd
SyphiN <35972897+SyphxN@users.noreply.github.com>
2024-02-17 18:48:05 -0500

    parent accounts can add children

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ import course
 import quiz
 
 Jim = student.Student("Jim","jimbro123","jimgalagher@ocdsb.ca","123456")
+Joel = student.Student("Joel","jojo3","jcher2@ocdsb.ca","124906")
 Mark= parent.Parent("Mark","markhamilfan","markgregory@gmail.com","696969")
 Mark.addChild(Jim.email)
 Kyle = teacher.Teacher("Kyle","Kyler69","kylesmith@ocdsb.ca","456789")
-
+Math = course.Course("Math","Math",Kyle.email,[Jim.email,Joel.email])
 print(dbRequests.GET("Accounts","Students"))
 print(dbRequests.GET("Accounts","Parents"))
 print(dbRequests.GET("Accounts","Teachers"))
