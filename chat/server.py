c077474ebade0db2c7c1ddec943073d3408f34e2
Joudat <joudat01@gmail.com>
2024-02-17 14:54:57 -0500

    Updated to TCP Chatroom

--- chat/server.py
+++ chat/server.py
@@ -1,23 +1,65 @@
 import socket
+import threading
+
+host = '127.0.0.1'
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server.bind(('localhost', 9999))
+server.bind((host, 9999))
 
 server.listen()
 
-client, addr = server.accept()
+clients = []
+nicknames = []
+# client, addr = server.accept()
 
-done = False
+# done = False
 
-while not done:
-    msg = client.recv(1024).decode('utf-8')
-    if msg == 'quit':
-        done = True
-    else:
-        print(msg)
-    client.send(input("Message: ").encode('utf-8'))
+# while not done:
+#     msg = client.recv(1024).decode('utf-8')
+#     if msg == 'quit':
+#         done = True
+#     else:
+#         print(msg)
+#     client.send(input("Message: ").encode('utf-8'))
 
-client.close()
+# client.close()
 
-server.close()
+# server.close()
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast(f'{nickname} has left the chat!'.encode('ascii'))
+            nicknames.remove(nickname)
+            break
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f"connected with {str(address)}")
+
+        client.send("NICK".encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f"Nickanme of client is {nickname}")
+        broadcast(f"{nickname} has joined the chat!".encode('ascii'))
+        client.send('Connected to the server!'.encode('ascii'))
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+print('Server is listening...')
+receive()
